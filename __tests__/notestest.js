0ffd8d8f4f671d2213aa678c78b5b9bd03c070ce
andrewtran5222 <andrewtran5222@gmail.com>
2024-05-15 00:59:32 -0700

    Updated tests

--- __tests__/notestest.js
+++ __tests__/notestest.js
@@ -1,20 +1,64 @@
 describe('Basic user flow for Website', () => {
-    // First, visit the lab 8 website
     beforeAll(async () => {
       await page.goto('https://andrewtran5222.github.io/CSE110-SP24-Lab6-Template');
     });
   
-    // Next, check to make sure that all 20 <product-item> elements have loaded
-    it('Initial Home Page - Check for 20 product items', async () => {
-      console.log('Checking for 20 product items...');
-      // Query select all of the <product-item> elements and return the length of that array
-      const numProducts = await page.$$eval('product-item', (prodItems) => {
-        return prodItems.length;
-      });
-      // Expect there that array from earlier to be of length 20, meaning 20 <product-item> elements where found
-      expect(numProducts).toBe(20);
+    const noteContent1 = 'Editing a new note';
+    test('Adding and Editing a new note and saving', async () => {
+        await page.click('.add-note');
+        await page.focus('.note');
+        await page.keyboard.type(noteContent1);
+        await page.keyboard.press('Tab'); // Save by pressing Tab
+        const savedNote = await page.$eval('.note', note => note.value);
+        expect(savedNote).toBe(noteContent1);
     });
-  
     
-  });
+    test('Editing an existing note and saving', async () => {
+        const noteContent2 = 'Editing an existing note and saving';
+        await page.focus('.note');
+        // Erase initial note content before editing
+        for(let i = 0; i < noteContent1.length; i++)
+        {
+            await page.keyboard.press('Backspace');
+        }
+        await page.keyboard.type(noteContent2);
+        await page.keyboard.press('Tab'); // Save by pressing Tab
+        const savedNote = await page.$eval('.note', note => note.value);
+        expect(savedNote).toBe(noteContent2);
+    });
+    
+    test('Notes are saved locally after refreshing page', async () => {
+        // Refresh page
+        await page.reload();
+        const savedNote = await page.$eval('.note', note => note.value);
+        expect(savedNote).not.toBe('');
+    });
+
+    test('Delete all notes with Ctrl + Shift + D', async () => {
+        // Trigger Ctrl + Shift + D
+        await page.keyboard.down('Control');
+        await page.keyboard.down('Shift');
+        await page.keyboard.press('KeyD');
+        await page.keyboard.up('Shift');
+        await page.keyboard.up('Control');
+      
+        // Check if no notes are present
+        const noteElements = await page.$$('.note');
+        expect(noteElements.length).toBe(0);
+    });
+    
+    test('Delete note by double clicking', async () => {
+        await page.click('.add-note');
+        await page.focus('.note');
+        await page.keyboard.type('Note to be deleted by double clicking');
+        await page.keyboard.press('Tab'); 
+        // Trigger Double Click
+        await page.evaluate(() => {
+          const note = document.querySelector('.note');
+          note.dispatchEvent(new MouseEvent('dblclick'));
+        });
+        const noteExists = await page.$('.note');
+        expect(noteExists).toBeNull();
+    });
+});
   
